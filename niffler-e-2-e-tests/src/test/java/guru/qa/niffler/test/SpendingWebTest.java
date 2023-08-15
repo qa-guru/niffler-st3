efca3070fc0ab161acd11696ef1231b4be49fc93
bagauov <bagauov@proitr.ru>
2023-08-15 19:50:21 +0300

    added category interface

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.Category;
 import guru.qa.niffler.jupiter.Spend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
@@ -15,6 +16,9 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class SpendingWebTest {
 
+    private final String username = "dima";
+    private final String category = "Пивко на яхте";
+
     static {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1980x1024";
@@ -24,16 +28,20 @@ public class SpendingWebTest {
     void doLogin() {
         Selenide.open("http://127.0.0.1:3000/main");
         $("a[href*='redirect']").click();
-        $("input[name='username']").setValue("dima");
+        $("input[name='username']").setValue(username);
         $("input[name='password']").setValue("12345");
         $("button[type='submit']").click();
     }
 
 
+    @Category(
+            username = username,
+            category = category
+    )
     @Spend(
-            username = "dima",
+            username = username,
             description = "Рыбалка на Ладоге",
-            category = "Рыбалка",
+            category = category,
             amount = 14000.00,
             currency = CurrencyValues.RUB
     )
