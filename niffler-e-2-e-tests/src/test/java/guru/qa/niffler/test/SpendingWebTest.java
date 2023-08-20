2470369f86023c678f05a22679f0a7cd95cb8395
i.bulakh <i.bulakh@webinar.ru>
2023-08-20 23:52:09 +0500

    ДЗ.1. Добавлена генерация категорий перед тестом

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.Category;
 import guru.qa.niffler.jupiter.Spend;
 import guru.qa.niffler.jupiter.User;
 import guru.qa.niffler.model.CurrencyValues;
@@ -10,7 +11,6 @@ import guru.qa.niffler.model.UserJson;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureId;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
@@ -19,36 +19,41 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.jupiter.User.UserType.WITH_FRIENDS;
 
-@Disabled
 public class SpendingWebTest extends BaseWebTest {
 
     static {
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1980x1024";
+        Configuration.browserSize = "1920x1080";
     }
 
-    private static final String user = "dima";
+    private static final String USERNAME = "dima";
+    private static final String PASSWORD = "12345";
+    private static final String CATEGORY = "Рыбалка";
 
     @BeforeEach
-    void doLogin(@User(userType = WITH_FRIENDS) UserJson userForTest) {
+    void doLogin() {
         Selenide.open("http://127.0.0.1:3000/main");
         $("a[href*='redirect']").click();
-        $("input[name='username']").setValue(userForTest.getUsername());
-        $("input[name='password']").setValue(userForTest.getPassword());
+        $("input[name='username']").setValue(USERNAME);
+        $("input[name='password']").setValue(PASSWORD);
         $("button[type='submit']").click();
     }
 
+    @Category(
+            username = USERNAME,
+            category = CATEGORY
+    )
+
     @Spend(
-            username = user,
+            username = USERNAME,
             description = "Рыбалка на Ладоге",
-            category = "Рыбалка",
+            category = CATEGORY,
             amount = 14000.00,
             currency = CurrencyValues.RUB
     )
     @Test
     @AllureId("100")
-    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend,
-                                                  @User(userType = WITH_FRIENDS) UserJson userForTest) {
+    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend) {
         $(".spendings__content tbody")
                 .$$("tr")
                 .find(text(createdSpend.getDescription()))
