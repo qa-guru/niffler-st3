b904b7442e16fc42adeb7c9b72140497dbef0dea
dtuchs <dtuchs@gmail.com>
2023-08-22 14:03:33 +0600

    vol 3
    (cherry picked from commit 08f215f070341c85fb879047ad2352f6c97142d1)

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -0,0 +1,65 @@
+package guru.qa.niffler.test;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.db.dao.AuthUserDAO;
+import guru.qa.niffler.db.dao.UserDataUserDAO;
+import guru.qa.niffler.db.model.Authority;
+import guru.qa.niffler.db.model.AuthorityEntity;
+import guru.qa.niffler.db.model.UserEntity;
+import guru.qa.niffler.jupiter.Dao;
+import guru.qa.niffler.jupiter.DaoExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Arrays;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+@ExtendWith(DaoExtension.class)
+public class LoginTest extends BaseWebTest {
+
+    @Dao
+    private AuthUserDAO authUserDAO;
+    @Dao
+    private UserDataUserDAO userDataUserDAO;
+    private UserEntity user;
+
+    @BeforeEach
+    void createUser() {
+        user = new UserEntity();
+        user.setUsername("valentin_2");
+        user.setPassword("12345");
+        user.setEnabled(true);
+        user.setAccountNonExpired(true);
+        user.setAccountNonLocked(true);
+        user.setCredentialsNonExpired(true);
+        user.setAuthorities(Arrays.stream(Authority.values())
+                .map(a -> {
+                    AuthorityEntity ae = new AuthorityEntity();
+                    ae.setAuthority(a);
+                    return ae;
+                }).toList());
+        authUserDAO.createUser(user);
+        userDataUserDAO.createUserInUserData(user);
+    }
+
+    @AfterEach
+    void deleteUser() {
+        userDataUserDAO.deleteUserByIdInUserData(user.getId());
+        authUserDAO.deleteUserById(user.getId());
+
+    }
+
+    @Test
+    void mainPageShouldBeVisibleAfterLogin() {
+        Selenide.open("http://127.0.0.1:3000/main");
+        $("a[href*='redirect']").click();
+        $("input[name='username']").setValue(user.getUsername());
+        $("input[name='password']").setValue(user.getPassword());
+        $("button[type='submit']").click();
+        $(".main-content__section-stats").should(visible);
+    }
+}
