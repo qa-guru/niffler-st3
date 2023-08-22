08f215f070341c85fb879047ad2352f6c97142d1
dtuchs <dtuchs@gmail.com>
2023-08-22 14:03:33 +0600

    vol 3

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/dao/AuthUserDAOHibernate.java
@@ -0,0 +1,17 @@
+package guru.qa.niffler.db.dao;
+
+import guru.qa.niffler.db.model.UserEntity;
+
+import java.util.UUID;
+
+public class AuthUserDAOHibernate implements AuthUserDAO {
+    @Override
+    public int createUser(UserEntity user) {
+        return 0;
+    }
+
+    @Override
+    public void deleteUserById(UUID userId) {
+
+    }
+}
