2393912a277fb21f9bfe81a6ea8b76426563f13d
qa-si <130176753+qa-si@users.noreply.github.com>
2023-08-23 20:00:24 +0300

    Написаны тесты

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/FriendsWebTest.java
@@ -11,6 +11,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static guru.qa.niffler.jupiter.user.User.UserType.INVITATION_SEND;
 import static guru.qa.niffler.jupiter.user.User.UserType.WITH_FRIENDS;
 
 public class FriendsWebTest extends BaseWebTest {
@@ -25,6 +26,15 @@ public class FriendsWebTest extends BaseWebTest {
 	}
 
 	@Test
+	@AllureId("100")
+	void friendsShouldBeDisplayedInTable100(@User(userType = WITH_FRIENDS) UserJson user1, @User(userType = INVITATION_SEND) UserJson user2){
+		$("[data-tooltip-id='friends']").click();
+		$$(".abstract-table tbody tr").findBy(text("You are friends")).shouldBe(visible);
+		$("[data-tooltip-id='people']").click();
+		$$(".abstract-table tbody tr").findBy(text("You are friends")).shouldBe(visible);
+	}
+
+	@Test
 	@AllureId("101")
 	void friendsShouldBeDisplayedInTable101(@User(userType = WITH_FRIENDS) UserJson userForTest){
 		$("[data-tooltip-id='friends']").click();
@@ -35,7 +45,7 @@ public class FriendsWebTest extends BaseWebTest {
 
 	@Test
 	@AllureId("102")
-	void friendsShouldBeDisplayedInTable102(@User(userType = WITH_FRIENDS) UserJson userForTest){
+	void friendsShouldBeDisplayedInTable102(){
 		$("[data-tooltip-id='friends']").click();
 		$$(".abstract-table tbody tr").findBy(text("You are friends")).shouldBe(visible);
 		$("[data-tooltip-id='people']").click();
