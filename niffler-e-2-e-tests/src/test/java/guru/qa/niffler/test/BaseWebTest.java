27c7782f7e210b9e7214b39551380efa2eef5140
ymorenov <VTB4090452@region.vtb.ru>
2023-08-23 16:24:18 +0300

    add hw2 user queue

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/BaseWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/BaseWebTest.java
@@ -1,7 +1,10 @@
 package guru.qa.niffler.test;
 
 import guru.qa.niffler.jupiter.WebTest;
+import guru.qa.niffler.page.LoginPage;
 
 @WebTest
 public abstract class BaseWebTest {
+
+    protected LoginPage loginPage = new LoginPage();
 }
