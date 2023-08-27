5ba751a901c1a8a464b0ad444c5709e8baa9fe41
gkorobov <korobovgk1@ya.ru>
2023-08-27 22:45:31 +0300

    dz3 - fix

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -1,20 +1,11 @@
 package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Selenide;
-import guru.qa.niffler.db.dao.AuthUserDAO;
-import guru.qa.niffler.db.dao.UserDataUserDAO;
-import guru.qa.niffler.db.model.Authority;
-import guru.qa.niffler.db.model.AuthorityEntity;
 import guru.qa.niffler.db.model.UserEntity;
-import guru.qa.niffler.jupiter.Dao;
-import guru.qa.niffler.jupiter.DaoExtension;
 import guru.qa.niffler.jupiter.annitation.DBUser;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import guru.qa.niffler.jupiter.extension.DaoExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.util.Arrays;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -23,7 +14,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class LoginTest extends BaseWebTest {
 
     @DBUser(username = "kolya_09",
-    password = "12345")
+            password = "12345")
     @Test
     void mainPageShouldBeVisibleAfterLogin(UserEntity user) {
         Selenide.open("http://127.0.0.1:3000/main");
